e8a9aecac4417b1830eca6c3e68a8eae47ae2684
ErexPL <zys.dawid@gmail.com>
2022-12-10 12:09:58 +0100

    Correct info checks and better UI in sign up

--- Sign_Up/sign_up.js
+++ Sign_Up/sign_up.js
@@ -1,16 +1,22 @@
 const inputFields = document.querySelectorAll('input');
 
-const registerBtn = document.querySelector(".registerBtn")
-const loginBtn = document.querySelector(".loginBtn")
-const moveBtn = document.querySelector(".moveBtn")
-const register = document.querySelector(".register")
-const login = document.querySelector(".login")
-const box = document.querySelector(".box")
-const signUp = document.querySelector(".signUp")
+const registerBtn = document.querySelector(".registerBtn");
+const loginBtn = document.querySelector(".loginBtn");
+const moveBtn = document.querySelector(".moveBtn");
+const register = document.querySelector(".register");
+const login = document.querySelector(".login");
+const box = document.querySelector(".box");
+const signUp = document.querySelector(".signUp");
 
-const email = document.querySelector("#email")
-const password1 = document.querySelector("#password1")
-const password2 = document.querySelector("#password2")
+const email = document.querySelector("#email");
+const password1 = document.querySelector("#password1");
+const password2 = document.querySelector("#password2");
+const username = document.querySelector("#username");
+
+let correctEmail = false;
+let correctPassword1 = false;
+let correctPassword2 = false;
+let correctUsername = false;
 
 loginBtn.addEventListener("click", ()=>{
     
@@ -45,6 +51,7 @@ registerBtn.addEventListener("click", ()=>{
 });
 
 inputFields.forEach(function(input) {
+
     input.addEventListener('keydown', function(event) {
     
         if (event.keyCode === 32) {
@@ -52,17 +59,76 @@ inputFields.forEach(function(input) {
         }
 
     });
+
+    input.addEventListener('click', function() {
+
+        if (input.style.borderBottomColor === 'red') {
+    
+            input.style.borderBottomColor = '';
+    
+        }
+    
+    });
+
 });
 
 function verify() {
 
+    if (username.value.length >= 4) {
+
+        correctUsername = true;
+
+    } else {
+
+        correctUsername = false;
+        if (username.value != '') {
+            username.style.borderBottomColor = 'red';
+        }
+
+    }
+
     if (email.value.endsWith("@gmail.com" || "@yahoo.com" || "@hotmail.com")) {
 
-        if (password1.value.length >= 8 && password1.value == password2.value) {
+        correctEmail = true;
 
-            window.location = '../Main/main.html';
-            
+    } else {
+
+        correctEmail = false;
+        if (email.value != '') {
+            email.style.borderBottomColor = 'red';
         }
+
+    }
+
+    if (password1.value.length >= 8) {
+
+        correctPassword1 = true;
+        
+    } else {
+
+        correctPassword1 = false;
+        if (password1.value != '') {
+            password1.style.borderBottomColor = 'red';
+        }
+
+    }
+
+    if (password1.value == password2.value) {
+
+        correctPassword2 = true;
+        
+    } else {
+
+        correctPassword2 = false;
+        if (password1.value != '') {
+            password2.style.borderBottomColor = 'red';
+        }
+
+    }
+
+    if (correctUsername == true && correctEmail == true && correctPassword1 == true && correctPassword2 == true) {
+
+        window.location = '../Main/main.html';
 
     }
 
