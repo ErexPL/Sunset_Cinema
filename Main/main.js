6e9ca75859c277255f9930a40b2704da1cd16667
ErexPL <zys.dawid@gmail.com>
2022-12-18 12:18:22 +0100

    Randomized movies

--- Main/main.js
+++ Main/main.js
@@ -1,5 +1,6 @@
 const filmContainer = document.querySelectorAll(".filmContainer");
-let trailerButton = document.querySelector(".trailerButton");
+let films = ["avengers1.png", "avengers2.png", "avengers3.png", "deadpool1.png", "deadpool2.png", "drstrange1.png", "drstrange2.png", "dzieci1.png", "dzieci2.png", "ironman1.png", "ironman2.png", "ironman3.png", "pacificrim1.png", "pacificrim2.png", "rocky1.png", "rocky2.png", "rocky3.png", "spiderman1.png", "spiderman2.png", "spiderman3.png", "thor1.png", "thor2.png", "thor3.png", "thor4.png"];
+let counter = 0;
 
 
 
@@ -19,5 +20,20 @@ filmContainer.forEach(film => {
         trailerButton.classList.remove("show");
     
     });
+
+    
+
+    if (!(counter % 6)) {
+
+        films = ["avengers1.png", "avengers2.png", "avengers3.png", "deadpool1.png", "deadpool2.png", "drstrange1.png", "drstrange2.png", "dzieci1.png", "dzieci2.png", "ironman1.png", "ironman2.png", "ironman3.png", "pacificrim1.png", "pacificrim2.png", "rocky1.png", "rocky2.png", "rocky3.png", "spiderman1.png", "spiderman2.png", "spiderman3.png", "thor1.png", "thor2.png", "thor3.png", "thor4.png"];
+        counter = 0;
+
+    }
+
+    filmImage = film.querySelector(".filmContainer > img");
+    randomMovie = Math.floor(Math.random() * films.length);
+    filmImage.src = "imgs/" + films[randomMovie];
+    films.splice(randomMovie, 1);
+    counter++;
 
 });
