1431bcf7511c4d0f55fc61b65cfee17bbff2a1de
ErexPL <zys.dawid@gmail.com>
2023-01-28 13:22:16 +0100

    Trying to do buy ticket

--- Main/main.js
+++ Main/main.js
@@ -1,5 +1,6 @@
 const filmContainer = document.querySelectorAll(".filmContainer");
-let films = ["avengers1.png", "avengers2.png", "avengers3.png", "deadpool1.png", "deadpool2.png", "drstrange1.png", "drstrange2.png", "dzieci1.png", "dzieci2.png", "ironman1.png", "ironman2.png", "ironman3.png", "pacificrim1.png", "pacificrim2.png", "rocky1.png", "rocky2.png", "rocky3.png", "spiderman1.png", "spiderman2.png", "spiderman3.png", "thor1.png", "thor2.png", "thor3.png", "thor4.png"];
+const buyTickets = document.querySelectorAll(".buyTicket");
+let films = ["avengers1.png", "avengers2.png", "avengers3.png", "avengers4.png", "deadpool1.png", "deadpool2.png", "drstrange1.png", "drstrange2.png", "grownups1.png", "grownups2.png", "ironman1.png", "ironman2.png", "ironman3.png", "pacificrim1.png", "pacificrim2.png", "rocky1.png", "rocky2.png", "spiderman1.png", "spiderman2.png", "spiderman3.png", "thor1.png", "thor2.png", "thor3.png", "thor4.png"];
 let counter = 0;
 
 
